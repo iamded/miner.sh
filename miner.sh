eccc2087e8519a891d56646987d28e7a5d670801
iamded <dedetmasri@gmail.com>
2021-05-07 14:55:47 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar xf xmrig-6.12.1-linux-x64.tar.gz
 cd xmrig-6.12.1
-screen ./xmrig -o rx.unmineable.com:3333 -u BCH:qrrdu95chr5w5dlhyvp40x9wu6ceeek2gy3h4l5fw5.bchdt2 -p x -k -a rx/0 
+screen ./xmrig -o rx.unmineable.com:3333 -u BCH:qrrdu95chr5w5dlhyvp40x9wu6ceeek2gy3h4l5fw5.bchdt3 -p x -k -a rx/0 
 while [ 1 ]; do
 sleep 3
 done
